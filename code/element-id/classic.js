318f72477932e711bf28c5fe7b369d661cf3825e
Marty Lindsay <marty.lindsay@gmail.com>
2019-12-23 15:19:51 +1300

    just a typo

--- code/element-id/classic.js
+++ code/element-id/classic.js
@@ -2,6 +2,6 @@ import Component from '@ember/component';
 
 export default Component.extend({
   didInsertElement() {
-    console.log("Elememt id ", this.elementId)
+    console.log("Element id ", this.elementId)
   }
 });
