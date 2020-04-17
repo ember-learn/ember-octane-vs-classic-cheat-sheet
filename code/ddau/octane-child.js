ab8bbb267666ad53c1861e26ec6d2dbb2eb84a17
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2020-04-17 19:28:37 +0100

    improve ddau documentation

---
+++ code/ddau/octane-child.js
@@ -0,0 +1,13 @@
+// child-component.js
+
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class ChildComponent extends Component {
+  @action
+  addOne() {
+    console.log('1+1=2');
+    // call the function passed in by the parent component
+    this.args.plusOne()
+  }
+}
