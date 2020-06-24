cd25b19849672fa151f3d6e6bbc93e68e350bd48
ijlee2 <ijlee2@users.noreply.github.com>
2020-06-23 21:15:37 -0500

    Updated image src to account for rootURL

---
+++ app/controllers/application.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import config from 'ember-octane-vs-classic-cheat-sheet/config/environment';
+
+export default class ApplicationController extends Controller {
+  rootURL = config.rootURL.replace(/\/$/, '');
+}
