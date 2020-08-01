26af5ea43d1f5b7ba29913146f31afd3049064b4
ijlee2 <ijlee2@users.noreply.github.com>
2020-08-01 08:25:40 -0500

    Upgraded ember-source from 3.19 to 3.20

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'ember-octane-vs-classic-cheat-sheet/app';
+import config from 'ember-octane-vs-classic-cheat-sheet/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
