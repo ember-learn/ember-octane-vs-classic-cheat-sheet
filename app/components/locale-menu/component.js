696268d7059e4095070c65c54eaaffc0aa3c5037
Simon Dahlbacka <simon.dahlbacka@ecraft.com>
2020-10-12 20:16:34 +0300

    Add possibility to select language

---
+++ app/components/locale-menu/component.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+
+export default class LocaleMenuComponent extends Component {
+  @service intl;
+
+  supportedLocales = ['en-US', 'fr-FR', 'pt-BR'];
+
+  @action
+  updateLocale(evt) {
+    if (evt.target.value) {
+      this.intl.setLocale(evt.target.value);
+    }
+  }
+}
