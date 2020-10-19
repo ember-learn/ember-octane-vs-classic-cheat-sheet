50fa9a54fa38909edfe633c5894b1c6208d028b4
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2020-10-18 17:24:45 -0700

    Fixed lint issue

--- app/components/guide-section/subsection/component.js
+++ app/components/guide-section/subsection/component.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 
 export default class SubsectionComponent extends Component {
