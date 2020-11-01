09ad67a93fdf4a80f37a4e18e3113f6f49ec0550
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-01 14:26:51 -0600

    Added tests for <LocaleMenu>

--- tests/integration/components/locale-menu/component-test.js
+++ tests/integration/components/locale-menu/component-test.js
@@ -1,16 +1,46 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
 
 module('Integration | Component | locale-menu', function(hooks) {
   setupRenderingTest(hooks);
+  setupIntl(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<LocaleMenu />`);
-    assert.dom('select option').exists({ count: 6 });
+  test('renders options for supported locales', async function(assert) {
+    await render(hbs`
+      <LocaleMenu />
+    `);
+
+    assert.dom('[data-test-field="Locale"]')
+      .hasValue('', 'The default option is selected.')
+
+    assert.dom('[data-test-option]')
+      .exists({ count: 5 }, 'There are 5 non-default options.');
+  });
+
+
+  test('allows the user to update the site locale', async function(assert) {
+    await render(hbs`
+      <LocaleMenu />
+    `);
+
+    await fillIn('[data-test-field="Locale"]', 'pt-BR');
+
+    assert.strictEqual(
+      this.intl.primaryLocale,
+      'pt-br',
+      'The site locale is pt-BR.'
+    );
+
+    await fillIn('[data-test-field="Locale"]', 'fr-FR');
+
+    assert.strictEqual(
+      this.intl.primaryLocale,
+      'fr-fr',
+      'The site locale is fr-FR.'
+    );
   });
 });
