26bb014fdc609f439f2f1fe8e52df6fd0284f84c
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-01 13:59:16 -0600

    Improved separation of concerns for <LocaleMenu>

---
+++ tests/unit/services/locale-test.js
@@ -0,0 +1,21 @@
+import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+module('Unit | Service | locale', function(hooks) {
+  setupTest(hooks);
+
+  test('menuOptions includes user-friendly labels', function(assert) {
+    const locale = this.owner.lookup('service:locale');
+
+    assert.deepEqual(
+      locale.menuOptions.map(({ label }) => label),
+      [
+        'English (US)',
+        'Français',
+        'Português (do Brasil)',
+        'Spanish',
+        '日本語',
+      ]
+    );
+  });
+});
