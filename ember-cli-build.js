33d7e0a1275305c6c4bfa7001c163cb8e797feac
Isaac Lee <ijlee2@users.noreply.github.com>
2021-03-27 09:07:06 +0100

    Updated ember-source to v3.25.4

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,7 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Theme options are coy, dark, funky, okaidia, solarizedlight, tomorrow,
     // and twilight. Remove the `theme` attribute to use the default theme.
